2e65a0372579b0c62d4ca125f9ffd864e561077d
nipungarg59 <nipungarg59@gmail.com>
2017-03-06 13:10:12 +0530

    Local database config

--- CodePlay/settings.py
+++ CodePlay/settings.py
@@ -25,6 +25,17 @@ def getDebugState():
         return False
     return True
 
+def getDatabaseDetails():
+    try:
+        from .credentials import databaseDetails
+        return databaseDetails()
+    except:
+        import dj_database_url
+        db_from_env = dj_database_url.config(conn_max_age=500)
+        # DATABASES['default'].update(db_from_env)
+        print (db_from_env)
+        return db_from_env
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -102,10 +113,7 @@ WSGI_APPLICATION = 'CodePlay.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': getDatabaseDetails()
 }
 
 
