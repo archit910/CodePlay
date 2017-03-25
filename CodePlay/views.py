c754a934c8f166a44dd081ae76fc3b7abd690bb1
nipungarg59 <nipungarg59@gmail.com>
2017-03-26 05:29:54 +0530

    Parsing Matrix

--- CodePlay/views.py
+++ CodePlay/views.py
@@ -7,11 +7,21 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 def parseMatrix(request):
-	return True
+	nodes = int(request.POST.get('nodes'))
+	matrix = []
+	for i in range(nodes):
+		row = []
+		for j in range(nodes):
+			variable = "matrix[" + str(i) + "][" + str(j)+"]"
+			row.append(int(request.POST.get(variable)))
+		matrix.append(row)
+	return matrix
 
 @csrf_exempt
 def solve(request):
 	returnResponse = {}
 	print(request.POST.get('matrix[0][0]'))
+	print ("here is the output=======================")
+	print(parseMatrix(request))
 	returnResponse['error'] = False
 	return JsonResponse(returnResponse)
